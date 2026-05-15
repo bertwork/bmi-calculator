af6683cbbdda26cd8f5138377a2338253118ee16
bertwork <adeniros24@gmail.com>
2026-05-16 00:08:36 +0800

    refactor(app): integrate UI class

--- headers/app.h
+++ headers/app.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "file_manager.h"
+#include "ui.h"
 
 #include <string>
 
 class App {
 private:
   FileManager file_manager;
+  UI ui;
 
 public:
   App(const std::string &db_folder = "database");
